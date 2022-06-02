1792c5b97a0dc1366f8c700367f6ce97303b2c05
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2022-06-02 16:28:07 +0900

    Fix a type error (string -> integer), and alignment mismatch (concatenating title and abstract).

--- convert.py
+++ convert.py
@@ -49,19 +49,22 @@ with gzip.open('../bioconcepts2pubtatorcentral.offset.gz', mode='rt', encoding='
         vals = l.split('|')
         pmid = vals[0]
         if len(vals[2]) > 0:
-          text = vals[2]
+          if len(text) > 0:
+            text += " "
+          text += vals[2]
       else:
         row = l.split('\t')
         pred = row[4].lower()
         id = row[0] + '_' + str(denotation_count)
         attr_id = row[0] + '_' + str(denotation_count) + '_' + row[4]
-        denotation = {"id" : id, "span" : {"begin" : row[1], "end" : row[2]}, "obj" : row[4]}
+        denotation = {"id" : id, "span" : {"begin" : int(row[1]), "end" : int(row[2])}, "obj" : row[4]}
 #        print(denotation)
         denotations.append(denotation)
         if len(row) == 6 and row[5] != '-':
           attribute  = {"id" : attr_id, "subj" : id, "pred" : pred, "obj" : row[5]}
 #          print(attribute)
           attributes.append(attribute)
+        denotation_count += 1
   print(']')
 
 f.close()
